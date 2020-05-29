4ab0d073d31dae695af3947c0df77c7fdc905615
Jcarri14 <j.carrion@students.salle.url.edu>
2020-05-29 02:59:15 +0200

    Update manage.py

--- src/manage.py
+++ src/manage.py
@@ -8,9 +8,6 @@ def main():
     dotenv.read_dotenv(override=True)
     
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'happyCall.settings')
-    #os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "C:/Users/win/Desktop/PersonalProjects/Python/DjangoChannelsTest/eltelefonodelaalegria-qflysh-411d2ce28cb7.json"
-    #os.environ['DIALOGFLOW_PROJECT_ID'] = "bso-agent-kdlbva"
-    #os.environ['DIALOGFLOW_PROJECT_ID'] = "eltelefonodelaalegria-qflysh"
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

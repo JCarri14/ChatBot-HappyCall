22941a227c305056d3a99212b29a0054bafcfaae
Jcarri14 <j.carrion@students.salle.url.edu>
2020-05-29 21:07:50 +0200

    fix: minor changes

--- src/chat/protocols/protocolsUtils.py
+++ src/chat/protocols/protocolsUtils.py
@@ -1,34 +1,5 @@
 from chat.models import *
 
-def checkPersonAttributes(user_input, params):
-    words = sentence_to_tokens(user_input)
-    print(words)
-    if params:
-        for param in params:
-            if param.key == "Person":
-                self.handlePersonValues(param.values)
-
-def handlePersonValues(values):
-    print(values)
-
-def checkOwnershipName(user_input):
-    if "mi nombre es" in user_input:
-        return True
-    if "me llamo" in user_input:
-        return True
-    if "soy" in user_input:
-        return True
-    return False
-
-def checkOwnershipValues(user_input):
-    if "uno de los" in user_input:
-        return False
-    if "una de las" in user_input:
-        return False
-    if "un grupo de" in user_input:
-        return False
-    return True
-
 def checkPersonName(params):
     for param in params:
         if param == "person.original":
@@ -50,9 +21,10 @@ def defaultPerson():
     coefficients = {}
     for c in (Coefficients):
         coefficients[c] = 0.0
-    p = Person(role=Roles.Transmitter,
+    return Person(role=Roles.Transmitter,
             healthContext = healthContext,
-            sentimentCoefficient = coefficients)
+            sentimentCoefficients = coefficients)
+    
 
 
 

6821d82d28aa6491c43da1975308b1afbb206905
Jcarri14 <j.carrion@students.salle.url.edu>
2020-05-30 03:50:45 +0200

    Update dfManager.py

--- src/chat/dialogflow_api/dfManager.py
+++ src/chat/dialogflow_api/dfManager.py
@@ -1,4 +1,5 @@
 import dialogflow as dialogflow
+import re
 
 
 class DialogflowManager:
@@ -32,10 +33,17 @@ class DialogflowManager:
         res = {}
         res['text'] = response.query_result.fulfillment_text
         res['intent'] = response.query_result.intent.display_name
-        res['params'] = response.query_result.parameters
+        #res['params'] = response.query_result.parameters
+        res['params'] = {}
+        word = re.compile('contexts/__system_counters__') 
         for context in response.query_result.output_contexts:
-            if "contexts/__system_counters__" in context.name:
-                res['params'] = response.query_result.output_contexts
+            if word.search(context.name):
+                res['params'] = context.parameters
+                break
+        word = re.compile('mood_') 
+        for param in response.query_result.parameters:
+            if word.search(param.lower()):
+                res['params'][param.lower()] = response.query_result.parameters[param]
         return res
 
 

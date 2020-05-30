465cbf5b82cc385cc8b2d26f6bfc040108d9600d
Jcarri14 <j.carrion@students.salle.url.edu>
2020-05-30 20:19:13 +0200

    Update urls.py

--- src/chat/urls.py
+++ src/chat/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('control/', views.dashboard, name='dashboard'),
+    path('control/<str:room_name>/', views.room_control, name='room-control'),
     path('<str:room_name>/', views.room, name='room'),
 ]

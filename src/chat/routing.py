dbaf32b2d92d289bfeff2ba5ff207fd674977eca
Jcarri14 <j.carrion@students.salle.url.edu>
2020-05-30 20:19:01 +0200

    Update routing.py

--- src/chat/routing.py
+++ src/chat/routing.py
@@ -4,5 +4,7 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
+    re_path(r'ws/chat/control/$', consumers.DashboardConsumer),
+    re_path(r'ws/chat/control/(?P<room_name>\w+)/$', consumers.ReadOnlyChatConsumer),
     re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer),
 ]

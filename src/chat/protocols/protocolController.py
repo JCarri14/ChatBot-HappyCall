a84fc85785c4995a6e71fa5ae8a8f530970277f7
Jcarri14 <j.carrion@students.salle.url.edu>
2020-05-30 20:04:38 +0200

    Update protocolController.py

--- src/chat/protocols/protocolController.py
+++ src/chat/protocols/protocolController.py
@@ -10,45 +10,49 @@ class ProtocolController:
     
     __instance = None
     __lang = "es"
+
+    
     
     class __Manager:
         
         def __init__(self, project_id, session_id):
             self.dfManager =  DialogflowManager(project_id, session_id, "es")
             self.dbManager = MongoODMManager("localhost", "27017", "happy_call")
-            self.conversation = Conversation(name=session_id)
-            self.emergency = Emergency(etype=EmergencyTypes.Normal)
-            self.emergency.num_involved = 1
-            self.contexts = []
-            #self.emergency.pers_involved = []
+            self.session = {
+                "conversation": Conversation(name=session_id),
+                "emergency": Emergency(etype=EmergencyTypes.Normal),
+                "witness": defaultPerson()
+            }
+            self.save_initial_session_values()
+        
+        def save_initial_session_values(self):
+            pId = self.dbManager.insert_person(self.session['witness'])
+            eId = self.dbManager.insert_emergency(self.session['emergency'])
+            self.session['conversation'].witness = pId
+            self.session['conversation'].emergencies.append(eId)
+            self.dbManager.insert_conversation(self.session['conversation']) 
 
-            self.dbManager.insert_conversation(self.conversation)
-            pId = self.dbManager.insert_person(defaultPerson())
-            self.emergency.pers_involved.append(pId)
-            eId = self.dbManager.insert_emergency(self.emergency)
-            self.dbManager.link_emergency_to_conversation(self.conversation.name, eId)
-    
     instance = None
 
     def __init__(self, project_id, session_id):
         if not ProtocolController.instance:
-            ProtocolController.instance = ProtocolController.__Manager(project_id, session_id)
-    
+            ProtocolController.instance = ProtocolController.__Manager(project_id, session_id)       
+
     def handle_input(self, text):
         if text == "END_CONVERSATION":
-            self.instance.conversation.finished_at = datetime.datetime.utcnow
+            self.instance.session['conversation'].finished_at = datetime.datetime.utcnow
             return ""
         else:
             info = self.instance.dfManager.request_fulfillment_text(text)
             res, flag = self.checkMoodInfo(info['params'])
             if flag == 1:
-                self.instance.dbManager.update_person_moods(self.instance.conversation.name, res)
-                calculateSentiment(self.instance.dbManager, self.instance.conversation.name, res, "")
+                self.instance.dbManager.update_witness_moods(self.instance.session['conversation'].name, res)
+                calculateSentiment(self.instance.dbManager, self.instance.session['conversation'].name, res, "")
             text_response = self.handle_intent(text, info)
             return text_response
 
     def checkMoodInfo(self, params):
-        moods = self.instance.dbManager.get_person_moods(self.instance.conversation.name)
+        moods = self.instance.dbManager.get_witness_moods(self.instance.session['conversation'].name)
         flag = 0
         word = re.compile('mood_') 
         point = re.compile(".")
@@ -74,9 +78,9 @@ class ProtocolController:
             "ProtocolWoundBase":self.woundBase,
             "FrasesMood":self.moodSentences,
             "GetPreferences":self.getPreferences,
+            "GetNombre":self.getNombre,
             "ProtocolSuicideAttempt":self.suicideAttempt,
             "ProtocolVictimIdentification":self.victimIdentification,
-            "GetPreferences":self.preferences,
             "DangerSuicide":self.dangerSuicide,
             "DangerOthers":self.dangerOthers
         }
@@ -91,8 +95,8 @@ class ProtocolController:
         numPersons = checkPersonsQuantity(params)
         p_name = checkPersonName(params)
         p = Person(name=p_name)
-        self.instance.emergency.pers_involved.append(p)
-        #self.instance.dbManager.update_emergencies_persons(self.instance.emergency.etype,self.instance.emergency.pers_involved)
+        self.instance.session['emergency'].pers_involved.append(p)
+        #self.instance.dbManager.update_emergencies_persons(self.instance.session['emergency'].etype,self.instance.session['emergency'].pers_involved)
         return result
 
     def agressionWithoutVictim(self, input, params, result):
@@ -106,10 +110,10 @@ class ProtocolController:
 
     def welcome(self, input, params, result):
         person_name = checkPersonName(params)
-        self.instance.emergency.pers_involved[0].name = person_name
+        self.instance.session['conversation'].witness.name = person_name
         #dbManager = MongoODMManager("localhost", "27017", "happy_call")
-        #self.instance.dbManager.update_emergencies_persons(self.instance.emergency.etype,
-                                                #self.instance.emergency.pers_involved)
+        #self.instance.dbManager.update_emergencies_persons(self.instance.session['emergency'].etype,
+                                                #self.instance.session['emergency'].pers_involved)
         return result
 
     def bleedingBase(self, input, params, result):
@@ -126,24 +130,15 @@ class ProtocolController:
         return result
 
     def moodSentences(self, input, params, result):
-    
-        words = [re.compile('healthCompleted') ,
-                re.compile('protocolCompleted'),
-                re.compile('followup'),
-                re.compile('dialog_context')]
-        flags = [0,0,0,0]
-        for c in self.instance.contexts:
-            print(c.name)
-            for i in range(len(words)):
-                if words[i].search(c.name):
-                    flags[i] = 1
-        self.instance.dfManager.set_contexts(self.instance.contexts)
-        print(flags)
-        if flags[3] == 1 or flags[2] == 1 or (flags[0] == 1 and flags[1] == 0) or (flags[0] == 0 and flags[1] == 1):
-            return result + ". Pero ahora centremonos en la pregunta anterior, por favor." 
+        result = self.handle_multiple_contexts(result)
         return result
     
     def getPreferences(self, input, params, result):
+        result = self.handle_multiple_contexts(result)
+        return result
+    
+    def getNombre(self, input, params, result):
+        result = self.handle_multiple_contexts(result)
         return result
 
     def victimIdentification(self, input, params, result):
@@ -152,13 +147,26 @@ class ProtocolController:
     def suicideAttempt(self, input, params, result):
         return result
 
-    def preferences(self, input, params, result):
-        return result
-
     def dangerSuicide(self, input, params, result):
         return result
 
     def dangerOthers(self, input, params, result):
+        return result
+    
+    def handle_multiple_contexts(self, result):
+        words = [re.compile('healthCompleted') ,
+                re.compile('protocolCompleted'),
+                re.compile('followup'),
+                re.compile('dialog_context')]
+        flags = [0,0,0,0]
+        for c in self.instance.contexts:
+            for i in range(len(words)):
+                if words[i].search(c.name):
+                    flags[i] = 1
+        self.instance.dfManager.set_contexts(self.instance.contexts)
+
+        if flags[3] == 1 or flags[2] == 1 or (flags[0] == 1 and flags[1] == 0) or (flags[0] == 0 and flags[1] == 1):
+            return result + ". Pero ahora centremonos en la pregunta anterior, por favor."
         return result
     
     

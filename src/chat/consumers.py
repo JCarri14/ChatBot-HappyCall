c4d5c4278cabba97fafda7da9315cc53ec6219f0
Jcarri14 <j.carrion@students.salle.url.edu>
2020-05-30 20:18:55 +0200

    Update consumers.py

--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -3,6 +3,7 @@ import json, os
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 from chat.protocols.protocolController import ProtocolController
+from chat.ddbb.MongoODMManager import MongoODMManager
 
 
 class ChatConsumer(WebsocketConsumer):
@@ -15,7 +16,6 @@ class ChatConsumer(WebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
-
         self.accept()
 
     def disconnect(self, close_code):
@@ -31,23 +31,127 @@ class ChatConsumer(WebsocketConsumer):
         message = text_data_json['message']
         project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
         session_id = self.room_group_name
-                
+        
         message = ProtocolController(project_id, session_id).handle_input(message) 
         
+        response = {
+            'sender': "bot-mssg",
+            'text': message
+        }
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': response
             }
         )
 
     # Receive message from room group
     def chat_message(self, event):
         message = event['message']
-
+        print("Arriva a aquiiiiiiiiiiiiiiiiiiiiii")
         # Send message to WebSocket
         self.send(text_data=json.dumps({
             'message': message
         }))
+
+
+class DashboardConsumer(WebsocketConsumer):
+    def connect(self):
+        #self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'control'
+        self.accept()
+
+    def disconnect(self, close_code):
+        print("Control Socker closing with code: " + str(close_code))
+
+    # Receive message from WebSocket
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['request']
+        
+        if "update" in message:
+            dbManager = MongoODMManager("localhost", "27017", "happy_call")
+            conversations = dbManager.get_conversations()
+            conversations = [cconversations[d]['name'] for d in conversations]
+            for c in conversations:
+                send_message("conversation", c)  
+
+    # Receive message from room group
+    def chat_message(self, event):
+        print()
+
+    def send_message(self, sender, text):
+        self.send(text_data=json.dumps({
+            'sender': sender,
+            'text': text
+        }))
+
+    def send_message_to_all(self, sender, text):
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'sender': sender,
+                'message': text
+            }
+        )
+
+class ReadOnlyChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+        
+        self.accept()
+
+    def disconnect(self, close_code):
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['request']
+        
+        #if "update" in message:
+            #print()
+            #TODO: get chats information
+            
+    # Receive message from room group
+    def chat_message(self, event):
+        response = {
+            'sender': "user-mssg",
+            'text': event['message']
+        }
+        # Send message to WebSocket
+        self.send(text_data=json.dumps({
+            'message': response
+        }))
+
+    def send_message(self, sender, text):
+        response = {
+            'sender': sender,
+            'text': text
+        }
+        # Send message to WebSocket
+        self.send(text_data=json.dumps({
+            'message': text
+        }))
+
+    def send_message_to_all(self, sender, text):
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'sender': sender,
+                'message': text
+            }
+        )

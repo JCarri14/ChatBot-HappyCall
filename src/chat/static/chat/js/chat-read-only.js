8f6b0bb6ca89d6d89647f6e67eb8fdc8fd4bed26
Jcarri14 <j.carrion@students.salle.url.edu>
2020-05-31 03:09:25 +0200

    Update chat-read-only.js

--- src/chat/static/chat/js/chat-read-only.js
+++ src/chat/static/chat/js/chat-read-only.js
@@ -11,9 +11,12 @@ const chatSocket = new WebSocket(
 
 chatSocket.onmessage = function(e) {
     const data = JSON.parse(e.data);
-    console.log(data);
+    
     if (data.message) {
         addListItem(data.message.sender, data.message.text);    
+    }
+    if (data.data) {
+        treatIncomingData(data.data);
     }
 };
 
@@ -31,4 +34,48 @@ function addListItem(className, text) {
     document.querySelector(".chat-messages ul").appendChild(mssgItem);
     var chat = document.querySelector(".chat-messages");
     chat.scrollTop = chat.scrollHeight;
+}
+
+function addSentimentItem(sentiment, value) {
+    var mssgItem = document.createElement("li");
+    mssgItem.classList.add("list-group-item");
+
+    var mssgValue = document.createElement("b");
+    mssgValue.appendChild(document.createTextNode(value));
+    mssgItem.appendChild( document.createTextNode(sentiment + ": "));
+    mssgItem.appendChild(mssgValue);
+
+    document.querySelector("#witness_sentiments").appendChild(mssgItem);
+}
+
+function removeAllChilds(element) {
+    var e = document.querySelector(element);   
+    //e.firstElementChild can be used. 
+    var child = e.lastElementChild;  
+    while (child) { 
+        e.removeChild(child); 
+        child = e.lastElementChild; 
+    }
+}
+
+function treatIncomingData(data) {
+    for (var key in data) {
+        console.log(data[key])
+        if (Array.isArray(data[key])) {
+            for (i in data[key]) {
+                document.getElementById(key).innerHTML += data[key][i] + "<br>";
+            }
+        } else {
+            if (typeof data[key] === "object") {
+                if (key == "witness_sentiments") {
+                    removeAllChilds("#witness_sentiments");
+                    for (var k in data[key]) {
+                        addSentimentItem(k, data[key][k]);
+                    }
+                }
+            } else {
+                document.getElementById(key).innerHTML = data[key];
+            }
+        }
+    }
 }

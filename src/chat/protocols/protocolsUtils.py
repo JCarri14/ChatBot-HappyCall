f9901571711114ba7b42aa677a61d2456e6cb3df
Jcarri14 <j.carrion@students.salle.url.edu>
2020-06-01 23:20:06 +0200

    Update protocolsUtils.py

--- src/chat/protocols/protocolsUtils.py
+++ src/chat/protocols/protocolsUtils.py
@@ -25,38 +25,48 @@ def checkPersonDescription(params):
     return checkParameters(["persondescription.original", "personDescription.original", "PersonDescription.original"], params)
 
 def checkPersonName(params):
-    return checkParameters(["UndefinedPerson.original", "undefinedperson.original", "person.original", "Person.original"], params)
+    return checkParameters(["UndefinedPerson", "undefinedperson", "person.original", "Person.original"], params)
 
 def checkPersonsQuantity(params):
-    return checkParameters(["UndefinedPerson.original", "undefinedperson.original", "number", "Number"], params)
+    return checkParameters(["UndefinedPerson", "undefinedperson", "number", "Number"], params)
 
 # -------------------------------------------------------------- #
 
 def loopDictParameters(result, search, params):
-    expr = re.compile("|".join(search))
-    for param in params:
-        if expr.search(param):
-            if isinstance(params[param], list):
-                if len(params[param]) > 0:
-                    result.extend(params[param])
-            elif params.get(param):
-                result.append(params[param])
+    expr = re.compile("|".join(search[2:]))
+
+    for items in params:
+        for key, value in items.items():
+            if key in search:
+                if isinstance(value, list):
+                    if len(value) > 0:
+                        result.append(value)
+                elif value:
+                    result.append(value)
     return result
 
 #Append: add list as value in the other one
 #Extend: add values to the corresponding list
 def checkParameters(search, params):
     result = []
+    #expr = re.compile("|".join(search))
     for param in params:
         if param in search:
-            if isinstance(params[param], dict):
-                result = loopDictParameters(result, search, params[param])
+            if param == "UndefinedPerson":
+                if not isinstance(params[param][0],str):
+                    result = loopDictParameters(result, search, params[param])
+                else:
+                    if "number" in search:
+                        result.append(1)
+                    else:   
+                        result.append(params[param][0])
             else:
-                if isinstance(params[param], list):
-                    if len(params[param]) > 0:
-                        result.extend(params[param])
+                try: p = list(params[param].items())
+                except: p = params[param]
+                if isinstance(p, list):
+                    result.append(p)
                 elif params.get(param):
-                    result.append(params[param])
+                    result.append(p)
     return result
 
 def checkIfWitnessProblem(user_input):
@@ -72,11 +82,11 @@ def defaultPerson(name="Person", role=Roles.Transmitter.value):
     for m in (Moods):
         disorders[m.value] = 0
     healthContext = HealthContext(disorders=disorders)
-
+    print("Disorders: ", disorders)
     coefficients = {}
     for c in (Coefficients):
         coefficients[c.value] = 0.0
-    return Person(role=role, name = name, healthContext = healthContext,
+    return Person(role=role, name=name, healthContext=healthContext,
                 sentimentCoefficients = coefficients)
     
 def defaultEmergency(etype=EmergencyTypes.Normal.value):

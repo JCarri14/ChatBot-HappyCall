da1f54e57adb3680e7f35e3c460401a3f47c1f96
Jcarri14 <j.carrion@students.salle.url.edu>
2020-06-01 03:51:42 +0200

    Update protocolsUtils.py

--- src/chat/protocols/protocolsUtils.py
+++ src/chat/protocols/protocolsUtils.py
@@ -1,4 +1,5 @@
 from chat.models import *
+import re
 
 #params.get(param)
 
@@ -26,6 +27,9 @@ def checkPersonPreferences(params):
 def checkPersonDescription(params):
     return checkParameters(["persondescription.original", "personDescription.original", "PersonDescription.original"], params)
 
+def checkUndefinedPersonName(params):
+    return checkParameters(["person.original"], params)
+
 def checkPersonName(params):
     return checkParameters(["person.original"], params)
 
@@ -45,6 +49,14 @@ def checkParameters(search, params):
                 result.append(params[param])
     return result
 
+def checkIfWitnessProblem(user_input):
+    values = ["Me estoy", "Estoy perdiendo", "Tengo un", "Tengo una", "Estoy sangrando"
+                "Llevo una hora sangrando", "Estoy sufriendo", "Me está", "Me han metido"]
+    expr = re.compile("|".join(values))
+    if expr.search(user_input):
+        return True
+    return False
+
 def defaultPerson(name="Person", role=Roles.Transmitter.value):
     disorders = {}
     for m in (Moods):
@@ -59,5 +71,4 @@ def defaultPerson(name="Person", role=Roles.Transmitter.value):
     
 def defaultEmergency():
     return Emergency(etype=EmergencyTypes.Normal, 
-                    num_involved=0, pers_involved=[], 
-                    location=[], is_active=True)
+                    num_involved=1, is_active=True)

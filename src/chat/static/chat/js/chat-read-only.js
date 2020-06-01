0b6f3c4e36e5b4a0065ee52f55b867d5e8a1586d
Jcarri14 <j.carrion@students.salle.url.edu>
2020-06-01 03:51:45 +0200

    Update chat-read-only.js

--- src/chat/static/chat/js/chat-read-only.js
+++ src/chat/static/chat/js/chat-read-only.js
@@ -48,6 +48,18 @@ function addSentimentItem(sentiment, value) {
     document.querySelector("#witness_sentiments").appendChild(mssgItem);
 }
 
+function createItem(parentName, name, value) {
+    var item = document.createElement("li");
+    item.classList.add("list-group-item");
+
+    var itemValue = document.createElement("b");
+    itemValue.setAttribute("id", parentName + "_" + name);
+    itemValue.appendChild(document.createTextNode(value));
+    item.appendChild(document.createTextNode(name + ": "));
+    item.appendChild(itemValue);
+    return item;
+}
+
 function removeAllChilds(element) {
     var e = document.querySelector(element);   
     //e.firstElementChild can be used. 
@@ -58,9 +70,64 @@ function removeAllChilds(element) {
     }
 }
 
+function toCapitalCase(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+function createBlock(title, content) {
+    var block = document.createElement("div");
+    block.classList.add("data-block");
+    block.classList.add("mb-4");
+    block.setAttribute("id", title);
+
+    var blockTitle = document.createElement("h4");
+    blockTitle.appendChild(document.createTextNode(toCapitalCase(title)));
+
+    var list = document.createElement("ul");
+    list.classList.add("list-group");
+
+    for (v in content) {
+        if (Array.isArray(content[v])) {
+            list.appendChild(createItem(title, toCapitalCase(v), content[v].join("<br>")));
+        } else {
+            list.appendChild(createItem(title, toCapitalCase(v), content[v]));
+        }
+    }
+    block.appendChild(blockTitle);
+    block.appendChild(list);
+    document.querySelector(".data-box").appendChild(block);
+}
+
+function update_block(key, values) {
+    var parent = document.querySelector(".data-box #" + key);
+    if (parent == null) {
+        createBlock(key, values);
+        return;
+    }
+    for (v in values) {
+        if (Array.isArray(values[v])) {
+            for (i in values[v]) {
+                document.getElementById(key+"_"+v).innerHTML = " ";
+                document.getElementById(key+"_"+v).innerHTML += values[value][i] + "<br>";
+            }
+        } else {
+            document.getElementById(key+"_"+v).innerHTML = " ";
+            doument.getElementById(key+"_"+v).innerHTML = values[value];
+        }
+    }
+}
+
 function treatIncomingData(data) {
+    for (item in data) {
+        console.log(data[item]);
+        if(data[item]) {
+            update_block(item, data[item]);
+        }
+    }
+}
+
+/*
     for (var key in data) {
-        console.log(data[key])
         if (Array.isArray(data[key])) {
             for (i in data[key]) {
                 document.getElementById(key).innerHTML += data[key][i] + "<br>";
@@ -78,4 +145,5 @@ function treatIncomingData(data) {
             }
         }
     }
-}
+
+*/
